210096df69f1aa5cf5b53b8f993ec190a21f5a9a
Rhedox <robin.kertels@gmail.com>
2016-05-30 01:34:17 +0200

    Fixed white status bar text (I hope at least)

--- scripts/app.ts
+++ scripts/app.ts
@@ -11,6 +11,8 @@
         private preferencesPage: PreferencesPage;
 
         public constructor() {
+
+
             this.framework = new Framework7({
                 sortable: false,
                 modalTitle: 'GesaHu VP'
@@ -23,6 +25,22 @@
             });
 
             this.checkPlatform();
+
+            // Check if a new cache is available on page load.
+            window.addEventListener('load', (e) => {
+
+                window.applicationCache.addEventListener('updateready', function (e) {
+                    if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
+                        // Browser downloaded a new app cache.
+                        this.framework.confirm('Eine neue Version der App steht zur Verfügung. Installieren?', 'Update', () => {
+                            window.location.reload();
+                        });
+                    } else {
+                        // Manifest didn't changed. Nothing new to server.
+                    }
+                }, false);
+
+            }, false);
 
             this.preferencesPage = new PreferencesPage(this.framework, this.mainView);
             this.mainPage = new MainPage(this.framework, this.mainView);
@@ -37,8 +55,8 @@
                 var link: string = 'https://play.google.com/store/apps/details?id=rhedox.gesahuvertretungsplan';
                 this.framework.confirm('Installiere die Gesahu VP Android App!', 'Diese Web-App ist für iOS optimiert.', () => {
                     var win = window.open(link);
-                    win.focus()
-                }, () => { });
+                    win.focus();
+                });
             } else if (this.framework.device.ios && !this.framework.device.webView) {
                 console.log('Not standalone!');
 
@@ -50,6 +68,10 @@
 
 //Start the app
 import Application = GesaHuVertretungsplan.Application;
+
+var statusBarMetas = document.getElementsByName("apple-mobile-web-app-status-bar-style");
+if (statusBarMetas.length > 0)
+    statusBarMetas[0].remove();
 
 window.onload = () => {
     var app = new Application();
