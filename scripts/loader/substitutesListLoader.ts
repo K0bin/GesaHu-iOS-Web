0eac81cbd9e21269083526bf94623fd1805f6ad2
Robin <robin.kertels@outlook.com>
2017-05-20 20:09:33 +0200

    Revert "Update loader to use JSON api"
    This reverts commit 8e7ee5684c9330a6222647219017da16ff502aaf.

--- scripts/loader/substitutesListLoader.ts
+++ scripts/loader/substitutesListLoader.ts
@@ -35,7 +35,7 @@
         public load(date: Date, student: Student): void {
             this.student = student;
 
-            let urlString = 'https://www.gesahui.de/home/mobil/appscripts/getvplan.php?day=' + date.getFullYear().toString() + '-' + (date.getMonth() + 1).toString() + '-' + date.getDate().toString();
+            let urlString = 'http://allow-any-origin.appspot.com/http://www.gesahui.de/home/view.php?d=' + date.getDate().toString() + '&m=' + (date.getMonth() + 1).toString() + '&y=' + date.getFullYear().toString();
 
             let ajax = new XMLHttpRequest();
             ajax.open('GET', urlString, true);
@@ -60,24 +60,162 @@
             }
         }
 
-        private parse(response: string): SubstitutesList {
-            let substitutes = new Array<Substitute>();
-            let vplan = JSON.parse(response);
-            for (var i = 0; i < vplan.Stunden.length; i++) {
-                let stunde = vplan.Stunden[i];
-                let substitute = new Substitute(
-                    stunde.Stundenanfang.toString().trim() + '-' + stunde.Stundeende.toString().trim(),
-                    stunde.Klasse.trim() + ' ' + this.resolver.resolveSubject(stunde.Fach),
-                    this.resolver.resolveTeacher(stunde.Lehrer),
-                    this.resolver.resolveTeacher(stunde.Vertretungslehrer),
-                    stunde.Raum.trim(),
-                    stunde.Hinweis.trim(),
-                    this.student);
 
+        //UGLY CODE INC
+        //the page that gets parsed is much worse though
+        private parse(response: string): SubstitutesList {
+            //console.log(response);
+
+            let substitutes = new Array<Substitute>();
+
+            response = response.replaceAll('bilder/logogray.png|bilder/schullogo.gif', '');
+
+            let root = document.createElement('div');
+            root.innerHTML = response;
+            let html = root.children;
+            let div: Element;
+            for (let i = 0; i < html.length; i++) {
+                let element = html.item(i);
+                if ((element.tagName && element.tagName.toLowerCase() == 'div') && (!element.id || element.id != 'cssmenu'))
+                    div = element;
+            }
+
+
+            let announcement: string = this.readAnnouncement(div);
+            let date: Date = this.readDate(div);
+
+
+            let tables = div.querySelectorAll('table');            
+            if (tables.length != 5)
+                return new SubstitutesList(substitutes, announcement, date);
+
+            let rows = tables[2].querySelectorAll('tr');
+
+            let lesson = '';
+            let subject = '';
+            let teacher = '';
+            let substituteTeacher = '';
+            let room = '';
+            let hint = '';
+
+            for (let rowI = 0; rowI < rows.length; rowI++) {
+                let row = rows[rowI];
+                let cells = row.querySelectorAll('td');
+
+                for (let i = 0; i < cells.length; i++) {
+                    let cell = cells[i];
+                    let text = cell.textContent.replaceAll(' +', ' ').trim();
+                    if (text) {
+                        switch (i) {
+                            case 0: {
+                                if (text != lesson && subject && lesson) {
+                                    let substitute = new Substitute(lesson.trim(), subject.trim(), teacher.trim(), substituteTeacher.trim(), room.trim(), hint.trim(), this.student);
+                                    substitutes.push(substitute);
+
+                                    subject = "";
+                                    teacher = "";
+                                    substituteTeacher = "";
+                                    room = "";
+                                    hint = "";
+                                }
+
+                                lesson = text.replaceAll(' ', '');
+                            } break;
+
+                            case 1: {
+                                if (subject) {
+                                    let substitute = new Substitute(lesson.trim(), subject.trim(), teacher.trim(), substituteTeacher.trim(), room.trim(), hint.trim(), this.student);
+                                    substitutes.push(substitute);
+
+                                    teacher = "";
+                                    substituteTeacher = "";
+                                    room = "";
+                                    hint = "";
+                                }
+
+                                let parts = text.split(' ');
+                                if (parts.length > 1)
+                                    text = this.resolver.resolveSubject(parts[0]) + " " + parts[1];
+
+                                subject = text;
+                            } break;
+
+                            case 2: {
+                                text = text.replaceAll(', |; |,+|;+', ',');
+                                let teachers = text.split(',');
+                                for (let _teacher of teachers) {
+                                    if (_teacher && _teacher != '---') {
+                                        if (teacher)
+                                            teacher += '; ';
+
+                                        teacher += this.resolver.resolveTeacher(_teacher.trim());
+                                    }
+                                }
+                            } break;
+
+                            case 3: {
+                                text = text.replaceAll(', |; |,+|;+', ',');
+                                let teachers = text.split(',');
+                                for (let _teacher of teachers) {
+                                    if (_teacher && _teacher != '---') {
+                                        if (substituteTeacher)
+                                            substituteTeacher += '; ';
+
+                                        substituteTeacher += this.resolver.resolveTeacher(_teacher.trim());
+                                    }
+                                }
+                            } break;
+
+                            case 4:
+                                if (text != '---')
+                                    room += text + ' ';
+                                break;
+
+                            case 5:
+                                if (text != '---')
+                                    hint += text + ' ';
+                                break;
+                        }
+                    }
+                }
+            }
+
+            if (subject) {
+                let substitute = new Substitute(lesson.trim(), subject.trim(), teacher.trim(), substituteTeacher.trim(), room.trim(), hint.trim(), this.student);
                 substitutes.push(substitute);
             }
 
-            return new SubstitutesList(substitutes, vplan.Hinweise, new Date(Date.parse(vplan.Datum)));
+            if (substitutes.length == 0 && !announcement && (!date || date == new Date()))
+                return null;
+
+
+            return new SubstitutesList(substitutes, announcement, date);
+        }
+
+        private readDate(element: Element): Date {
+            let dateElement = element.querySelector('center>div>div>center>table[id="AutoNumber1"]>tbody>tr>td>p>font[size="4"][face="Arial"]');
+            if (!dateElement || !dateElement.textContent)
+                return null;
+
+            let strings = dateElement.innerHTML.split('<br>');
+            if (strings.length < 3)
+                return null;
+
+            let dateString = strings[2];
+            let dateStringParts = dateString.substr(dateString.indexOf(' ')).split('.');
+
+            if (dateStringParts.length < 3)
+                return null;
+
+            return new Date(parseInt(dateStringParts[2]), parseInt(dateStringParts[1]) - 1, parseInt(dateStringParts[0]));
+        }
+
+        private readAnnouncement(element: Element): string {
+            let announcementElement = element.querySelector('center>div>p>b>font[size="2"][face="Arial"]');
+            if (!announcementElement || !announcementElement.textContent)
+                return null;
+
+            return announcementElement.textContent;
         }
     }
 }
