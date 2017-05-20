fe5d37d89203b4977c00aed10b85d319c4ce70ac
Robin <robin.kertels@outlook.com>
2017-05-20 20:10:24 +0200

    Revert "Include Firebase"
    This reverts commit 5ea876bfae5c9d01121c50eb3939b25819f9280e.

--- scripts/app.ts
+++ scripts/app.ts
@@ -1,6 +1,4 @@
-﻿/// <reference path="../.packages/framework7/ts/framework7.d.ts" />
-
-module GesaHuVertretungsplan {
+﻿module GesaHuVertretungsplan {
     import MainPage = GesaHuVertretungsplan.Ui.MainPage;
     import AboutPage = GesaHuVertretungsplan.Ui.AboutPage;
     import PreferencesPage = GesaHuVertretungsplan.Ui.PreferencesPage;
@@ -13,6 +11,8 @@ module GesaHuVertretungsplan {
         private preferencesPage: PreferencesPage;
 
         public constructor() {
+
+
             this.framework = new Framework7({
                 sortable: false,
                 modalTitle: 'GesaHu VP'
@@ -27,12 +27,12 @@ module GesaHuVertretungsplan {
             this.checkPlatform();
 
             // Check if a new cache is available on page load.
-            var _this = this;
             window.addEventListener('load', (e) => {
+
                 window.applicationCache.addEventListener('updateready', function (e) {
                     if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
                         // Browser downloaded a new app cache.
-                        _this.framework.confirm('Eine neue Version der App steht zur Verfügung. Installieren?', 'Update', () => {
+                        this.framework.confirm('Eine neue Version der App steht zur Verfügung. Installieren?', 'Update', () => {
                             window.location.reload();
                         });
                     } else {
