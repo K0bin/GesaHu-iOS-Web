ece45bad7f435d135ea5ecf72c4da3ffcf7e7209
Robin <robin.kertels@outlook.com>
2018-03-22 21:11:57 +0100

    Use (not firebase) cors proxy

--- src/model/repository/api/apiSubstitutesRepository.ts
+++ src/model/repository/api/apiSubstitutesRepository.ts
@@ -6,14 +6,19 @@ class ApiSubstitutesRepository implements SubstitutesRepository {
     constructor() {}
 
     load(date: Date, callback: (date: Date, substitutes: Substitute[]) => void): void {
-        let urlString = 'https://www.gesahui.de/home/mobil/appscripts/getvplan.php?day=' + date.getFullYear().toString() + '-' + (date.getMonth() + 1).toString() + '-' + date.getDate().toString();
+        let apiUrl = 'https://www.gesahui.de/home/mobil/appscripts/getvplan.php?day=' + date.getFullYear().toString() + '-' + (date.getMonth() + 1).toString() + '-' + date.getDate().toString();
+        let urlString = 'https://crossorigin.me/'+apiUrl; //CORS proxy
         console.log(urlString);
 
-        fetch(urlString)
-            .then(response => response.json())
-            .then(json => deserialize(json))
-            .then(data => callback(data[0], data[2]))
-            .catch(error => console.error("BLA:"+error));
+        fetch(urlString, {
+            headers: {
+                Origin: 'GesaHu App'
+            }
+        })
+        .then(response => response.json())
+        .then(json => deserialize(json))
+        .then(data => callback(data[0], data[2]))
+        .catch(error => console.error(error));
     }
 }
 
