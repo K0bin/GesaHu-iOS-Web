d6fffe06c75a3ae87e40b12579f5ec65def092bf
Robin <robin.kertels@outlook.com>
2018-03-22 23:22:38 +0100

    Convert windows-1252 encoding

--- src/model/repository/api/apiSubstitutesRepository.ts
+++ src/model/repository/api/apiSubstitutesRepository.ts
@@ -2,23 +2,39 @@ import SubstitutesRepository from '../substitutesRepository'
 import Substitute from '../../substitute'
 import { deserialize } from './substitutesListDeserializer'
 
+const windows1252 = '\u0000\u0001\u0002\u0003\u0004\u0005\u0006\u0007\b\t\n\u000b\f\r\u000e\u000f\u0010\u0011\u0012\u0013\u0014\u0015\u0016\u0017\u0018\u0019\u001a\u001b\u001c\u001d\u001e\u001f !"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~�€�‚ƒ„…†‡ˆ‰Š‹Œ�Ž��‘’“”•–—˜™š›œ�žŸ ¡¢£¤¥¦§¨©ª«¬­®¯°±²³´µ¶·¸¹º»¼½¾¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûüýþÿ';
 class ApiSubstitutesRepository implements SubstitutesRepository {
     constructor() {}
 
+    //private corsProxy = 'http://allow-any-origin.appspot.com/';
+    private corsProxy = 'https://crossorigin.me/';
+    //private corsProxy = 'https://cors-anywhere.herokuapp.com/'
+
     load(date: Date, callback: (date: Date, substitutes: Substitute[]) => void): void {
         let apiUrl = 'https://www.gesahui.de/home/mobil/appscripts/getvplan.php?day=' + date.getFullYear().toString() + '-' + (date.getMonth() + 1).toString() + '-' + date.getDate().toString();
-        let urlString = 'https://crossorigin.me/'+apiUrl; //CORS proxy
+        let urlString = this.corsProxy+apiUrl; //CORS proxy
         console.log(urlString);
 
         fetch(urlString, {
             headers: {
-                Origin: 'GesaHu App'
+                'Origin': 'GesaHu App'
             }
         })
-        .then(response => response.json())
+        .then(response => response.arrayBuffer())
+        .then(buffer => this.fromWindows1252Buffer(buffer))
+        .then(text => JSON.parse(text))
         .then(json => deserialize(json))
         .then(data => callback(data[0], data[2]))
         .catch(error => console.error(error));
+    }
+
+    private fromWindows1252Buffer(buffer: ArrayBuffer): string {
+        let array = new Uint8Array(buffer);
+        let text = '';
+        for (var i = 0; i < array.length; i++) {
+            text += windows1252.charAt(array[i]);
+        }
+        return text;
     }
 }
 
